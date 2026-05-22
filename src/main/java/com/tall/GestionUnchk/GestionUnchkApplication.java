56e1bfb2d9f46a142a8b63a4897409936f41873a
mor tall <tallpape32@gmail.com>
2026-05-22 01:47:16 +0000

    initialisation du projet

---
+++ src/main/java/com/tall/GestionUnchk/GestionUnchkApplication.java
@@ -0,0 +1,13 @@
+package com.tall.GestionUnchk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUnchkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUnchkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tall/GestionUnchk/GestionUnchkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tall.GestionUnchk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUnchkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
